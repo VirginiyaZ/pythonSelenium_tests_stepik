b0671d9393601213a7884af4ff2daa7056ac0f11
Virgy <email@gmail.com>
2022-08-22 14:42:01 +0500

    unittest corrections commit

--- module3/test_file_unittest.py
+++ module3/test_file_unittest.py
@@ -7,30 +7,32 @@ import time
 class TestAll(unittest.TestCase):
 
     def test_check_registration1(self):
+        browser = webdriver.Chrome()
         self.browser = browser.get(
             "http://suninjuly.github.io/registration1.html"
         )
 
         self.first = browser.find_element(By.CSS_SELECTOR, "div.first_block input.first"
-        ).send_keys("Data")
+        ).send_keys("Hi")
 
         self.second = browser.find_element(By.CSS_SELECTOR,
             "div.first_block input.second"
-        ).send_keys("Data")
+        ).send_keys("World")
 
         self.third = browser.find_element(By.CSS_SELECTOR,
             "div.first_block input.third"
-        ).send_keys("Data")
+        ).send_keys("mail@mail.com")
 
         self.button = browser.find_element(By.CSS_SELECTOR,
             "button.btn"
         ).click()
 
-        self.assertEqual("Поздравляем! Вы успешно зарегистировались!",
+        self.assertEqual("Congratulations! You have successfully registered!",
                          browser.find_element(By.TAG_NAME, "h1").text
                          )
 
     def test_check_registration2(self):
+        browser = webdriver.Chrome()
         self.browser = browser.get(
             "http://suninjuly.github.io/registration2.html"
         )
@@ -51,12 +53,9 @@ class TestAll(unittest.TestCase):
             "button.btn"
         ).click()
 
-        self.assertEqual("Поздравляем! Вы успешно зарегистировались!",
+        self.assertEqual("Congratulations! You have successfully registered!",
                          browser.find_element(By.TAG_NAME, "h1").text
                          )
 
-
 if __name__ == '__main__':
-    browser = webdriver.Chrome()
-    browser.implicitly_wait(5)
     unittest.main()
